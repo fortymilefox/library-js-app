5e1acf1c3e36ff924f7c1abf2e4e8dcefe2716ed
Keith Gordon <a.keithgordon27@gmail.com>
2021-07-17 21:25:47 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read){
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read){
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  return myLibrary;
+  
+}
