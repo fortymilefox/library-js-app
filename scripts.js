cccb7f0c63c68128559cddeab476301f819773c7
Keith Gordon <a.keithgordon27@gmail.com>
2021-07-22 14:18:41 -0400

    refactor functions for adding book

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const libraryList = document.querySelector('#library-list');
 let myLibrary = [];
 
 function Book(title, author, pages, read){
@@ -7,9 +8,43 @@ function Book(title, author, pages, read){
   this.read = read;
 }
 
+function viewLibrary() {
+  //libraryList.textContent = '';
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    const entryRow = document.createElement('tr');
+    entryRow.classList.add('library-entry');
+    libraryList.appendChild(entryRow);
+
+    //Title
+    const bookTitle = document.createElement('td');
+    bookTitle.textContent = myLibrary[i].title;
+    entryRow.appendChild(bookTitle);
+
+    //Author
+    const bookAuthor = document.createElement('td');
+    bookAuthor.textContent = myLibrary[i].author;
+    entryRow.appendChild(bookAuthor);
+
+    //Pages
+    const bookPages = document.createElement('td');
+    bookPages.textContent = myLibrary[i].pages;
+    entryRow.appendChild(bookPages);
+
+    //Read
+    const bookRead = document.createElement('td');
+    bookRead.
+  }
+}
+
 function addBookToLibrary(title, author, pages, read){
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
-  return myLibrary;
+  viewLibrary();
+  //console.log(myLibrary);
   
 }
+
+
+//viewLibrary();
+addBookToLibrary('Jane Eyre', 'Charlotte Bronte', '532', 'read');
+
